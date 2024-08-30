1da08e03d76f7a8bdd97127089f6e4ea66daf986
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-08-31 02:24:02 +0600

    my list data loaded

--- client/src/Pages/MyList/MyList.jsx
+++ client/src/Pages/MyList/MyList.jsx
@@ -1,14 +1,40 @@
-import { useEffect } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
+import TableRow from "../../Components/TableRow/TableRow";
 
 
 const MyList = () => {
-    useEffect(()=>{
-        fetch("")
-    },[])
+    const { user } = useContext(AuthContext);
+    const [myData, setMyData] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:3000/spots/${user.email}`)
+            .then(res => res.json())
+            .then(data => setMyData(data))
+    }, [user])
     return (
         <div>
-            My List
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+
+
+                        {
+                            myData?.map(data => <TableRow data={data} key={data._id}></TableRow>)
+                        }
+                        
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
